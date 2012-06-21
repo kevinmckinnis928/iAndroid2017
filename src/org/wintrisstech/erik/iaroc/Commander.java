880b9f37006dadb55b2ca0732417ec98156e3a9f
Kevin McKinnis <kevinmckinnis928@gmail.com>
2012-06-20 20:30:55 -0700

    need fixing with correction drive

--- src/org/wintrisstech/erik/iaroc/Commander.java
+++ src/org/wintrisstech/erik/iaroc/Commander.java
@@ -11,8 +11,8 @@ import org.wintrisstech.irobot.ioio.IRobotCreateScript;
 import org.wintrisstech.sensors.UltraSonicSensors;
 
 public class Commander extends IRobotCreateAdapter {
-    
-    public static final int INCREASED_SPEED = 110;
+
+    public static final int INCREASED_SPEED = 101;
     public static final int STANDARD_SPEED = 100;
     private static final String TAG = "Lada";
     private final Dashboard dashboard;
@@ -30,9 +30,9 @@ public class Commander extends IRobotCreateAdapter {
     private final int FRONT_WALL_PRESENT_SONAR_VALUE = 2000;
     private final int LEFT_WALL_PRESENT_SONAR_VALUE = 2000;
     private final int CORRECTION_EQUILIBRIUM = 836;
-    
+
     public Commander(IOIO ioio, IRobotCreateInterface create, Dashboard dashboard) throws ConnectionLostException {
-        
+
         super(create);
         this.ioio = ioio;
         this.dashboard = dashboard;
@@ -41,9 +41,9 @@ public class Commander extends IRobotCreateAdapter {
                     58, 10
                 });
     }
-    
+
     public void initialize() throws ConnectionLostException {
-        
+
         dashboard.log("===========Initialize===========");
         readSensors(SENSORS_GROUP_ID6);//Resets all counters in the Create to 0.
         dashboard.log("Battery Voltage" + getVoltage());//reads the voltage of the Robot
@@ -60,23 +60,23 @@ public class Commander extends IRobotCreateAdapter {
             SystemClock.sleep(1000);
             return;
         }
-        
+
         SystemClock.sleep(1000);
-        
+
         readPrintUltraSonic();
-        
+
         SystemClock.sleep(1000);
-        
+
         readPrintUltraSonic();
-        
+
         scenarios();
-        
+
         if (isBumping()) {
             driveDirect(0, 0);
         }
-        SystemClock.sleep(1000);        
+        SystemClock.sleep(1000);
     }
-    
+
     private void readPrintUltraSonic() {
         try {
             sonar.readUltrasonicSensors();
@@ -90,13 +90,13 @@ public class Commander extends IRobotCreateAdapter {
         dashboard.log("Right Sonar" + sonar.getRightDistance());// reads the distance from the right wall
         dashboard.log("--------------------------------------");
     }
-    
+
     public void stop() throws ConnectionLostException {
-        
+
         dashboard.log("===========Stop===========");
         driveDirect(0, 0); // stop the Create
     }
-    
+
     private void scenarios() throws ConnectionLostException {
 
         //scenario 1
@@ -138,9 +138,9 @@ public class Commander extends IRobotCreateAdapter {
             scenario8();
         }
     }
-    
+
     public void turn(int angleToTurn, int directionToTurn) throws ConnectionLostException {
-        
+
         dashboard.log("Engine.turn(" + angleToTurn + ", " + directionToTurn + ");");
         int angleSoFar = 0;
         drive(directionToTurn);
@@ -152,7 +152,7 @@ public class Commander extends IRobotCreateAdapter {
         }
         stop();
     }
-    
+
     public void turnUsingScript(int direction) throws ConnectionLostException {
         IRobotCreateScript turnUsingScript = new IRobotCreateScript();
         if (direction == RIGHT) {
@@ -175,13 +175,13 @@ public class Commander extends IRobotCreateAdapter {
         byte[] script = turnUsingScript.getBytes();
         dashboard.log("Should stop now");
         playScript(script, false);
-        
+
     }
-    
+
     private void drive(int direction) throws ConnectionLostException {
         drive(direction, 0, 0);
     }
-    
+
     private void drive(int direction, int leftSpeedUp, int rightSpeedUp) throws ConnectionLostException {
         if (direction == FORWARD) {
             driveDirect(leftSpeedUp, rightSpeedUp);
@@ -193,7 +193,7 @@ public class Commander extends IRobotCreateAdapter {
             throw new RuntimeException("I don't know how to go that way!");
         }
     }
-    
+
     private void scenario5() throws ConnectionLostException {
         double x = Math.random();
         if (x <= .5) {
@@ -204,7 +204,7 @@ public class Commander extends IRobotCreateAdapter {
             driveDistance(speed, oneBlockDistance);
         }
     }
-    
+
     private void scenario6() throws ConnectionLostException {
         double x = Math.random();
         if (x <= .5) {
@@ -214,7 +214,7 @@ public class Commander extends IRobotCreateAdapter {
             driveDistance(speed, oneBlockDistance);
         }
     }
-    
+
     private void scenario7() throws ConnectionLostException {
         double x = Math.random();
         if (x <= .5) {
@@ -224,7 +224,7 @@ public class Commander extends IRobotCreateAdapter {
             driveDistance(speed, oneBlockDistance);
         }
     }
-    
+
     private void scenario8() throws ConnectionLostException {
         double x = Math.random();
         if (x <= .33) {
@@ -237,23 +237,29 @@ public class Commander extends IRobotCreateAdapter {
             driveDistance(speed, oneBlockDistance);
         }
     }
+
+    private void driveDistanceScript()
+    {
+        
+    }
     
     private void driveDistance(int speed, int distance) throws ConnectionLostException {
         int distanceDriven = 0;
         while (distanceDriven < distance) {
             readSensors(SENSORS_DISTANCE);
             distanceDriven += getDistance();
-            try {
-                driveCorrection();
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Commander.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            drive(FORWARD, speed, speed);
+            //try {
+            //    driveCorrection();
+            //} catch (InterruptedException ex) {
+            //    Logger.getLogger(Commander.class.getName()).log(Level.SEVERE, null, ex);
+            //}
         }
-        
+
     }
-    
+
     private void driveUntilBump() throws ConnectionLostException {
-        
+
         while (!isBumping()) {
             driveDirect(STANDARD_SPEED, STANDARD_SPEED);
             logDistance();
@@ -261,23 +267,23 @@ public class Commander extends IRobotCreateAdapter {
         stop();
         done = true;
     }
-    
+
     private boolean isBumping() throws ConnectionLostException {
         readSensors(SENSORS_BUMPS_AND_WHEEL_DROPS);
         return isBumpLeft() || isBumpRight();
     }
-    
+
     private void logDistance() throws ConnectionLostException {
         readSensors(SENSORS_DISTANCE);
         distance += getDistance();
         dashboard.log("Distance" + distance);
         readPrintUltraSonic();
     }
-    
+
     private boolean isProgramDone() {
         return done;
     }
-    
+
     private void driveCorrection() throws ConnectionLostException, InterruptedException {
         sonar.readUltrasonicSensors();
         int leftSonarValue = sonar.getLeftDistance();
@@ -285,75 +291,75 @@ public class Commander extends IRobotCreateAdapter {
         dashboard.log("left sonar value " + leftSonarValue);
         dashboard.log("right sonar value " + rightSonarValue);
         dashboard.log("---------------------------------------");
-        
+
         if (isLeftWallThere(leftSonarValue) && isRightWallThere(rightSonarValue)) {
-            
+
             dashboard.log("Center the robot using both walls.");
             dashboard.log("------------------------------------");
-            
+
             if (leftSonarValue > rightSonarValue) {
-                
+
                 dashboard.log("Speed up the right wheel.");
-                drive(FORWARD, STANDARD_SPEED, INCREASED_SPEED);
-                
-            } else if (leftSonarValue < rightSonarValue) {
-                
-                dashboard.log("Speed up the left wheel.");
                 drive(FORWARD, INCREASED_SPEED, STANDARD_SPEED);
-                
+
+            } else if (leftSonarValue < rightSonarValue) {
+
+                dashboard.log("Speed up the left wheel.");
+                drive(FORWARD, STANDARD_SPEED, INCREASED_SPEED);
+
             } else {
-                
+
                 dashboard.log("The robot is centered.  Nothing to do.");
             }
-            
+
         } else if (isLeftWallThere(leftSonarValue)) {
-            
+
             dashboard.log("Center the robot using left wall.");
             dashboard.log("------------------------------------");
-            
-            
+
+
             if (leftSonarValue > CORRECTION_EQUILIBRIUM) {
-                
+
                 dashboard.log("Speed up the right wheel.");
-                drive(FORWARD, STANDARD_SPEED, INCREASED_SPEED);
-                
-            } else {
-                
-                dashboard.log("Speed up the left wheel.");
                 drive(FORWARD, INCREASED_SPEED, STANDARD_SPEED);
+
+            } else {
+
+                dashboard.log("Speed up the left wheel.");
+                drive(FORWARD, STANDARD_SPEED, INCREASED_SPEED);
             }
-            
+
         } else if (isRightWallThere(rightSonarValue)) {
-            
+
             dashboard.log("Center the robot using the right wall.");
             dashboard.log("------------------------------------");
-            
+
             if (rightSonarValue > CORRECTION_EQUILIBRIUM) {
-                
+
                 dashboard.log("Speed up the left wheel.");
-                drive(FORWARD, INCREASED_SPEED, STANDARD_SPEED);
-                
-            } else {
-                
-                dashboard.log("Speed up the right wheel.");
                 drive(FORWARD, STANDARD_SPEED, INCREASED_SPEED);
+
+            } else {
+
+                dashboard.log("Speed up the right wheel.");
+                drive(FORWARD, INCREASED_SPEED, STANDARD_SPEED);
             }
         } else {
-            
+
             dashboard.log("No data to center the robot.");
             dashboard.log("------------------------------------");
         }
         SystemClock.sleep(1000);
     }
-    
+
     private boolean isRightWallThere(int sonarValue) {
         return sonarValue < RIGHT_WALL_PRESENT_SONAR_VALUE;
     }
-    
+
     private boolean isFrontWallThere(int sonarValue) {
         return sonarValue < FRONT_WALL_PRESENT_SONAR_VALUE;
     }
-    
+
     private boolean isLeftWallThere(int sonarValue) {
         return sonarValue < LEFT_WALL_PRESENT_SONAR_VALUE;
     }
